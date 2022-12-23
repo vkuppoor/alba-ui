0950c1215c607527986eb555da8cf6acbb03585b
Venkat_Kuppoor <venkkuppoor@gmail.com>
2022-12-23 12:38:17 -0500

    removed unnecessary stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../projects/**/*.stories.mdx",
     "../projects/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
