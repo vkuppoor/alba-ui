b7de86b55b7ba9c550c091d945b306bec66fd7dd
Venkat_Kuppoor <venkkuppoor@gmail.com>
2022-12-30 11:06:27 -0500

    Implemented tailwindcss; reverted a commit revert

--- projects/alba-ui/src/lib/components/button/alba-button.stories.ts
+++ projects/alba-ui/src/lib/components/button/alba-button.stories.ts
@@ -13,7 +13,7 @@ storiesOf('Alba Button', module)
   component: AlbaButtonComponent,
   template: `
     <div style="display: flex; column-gap: .25rem;">
-      <alba-button (click)="onClick()" >Primary</alba-button>
+    <alba-button (click)="onClick()" >Primary</alba-button>
       <alba-button variant="secondary">Secondary</alba-button>
       <alba-button variant="success">Success</alba-button>
       <alba-button variant="info">Info</alba-button>
@@ -48,8 +48,8 @@ storiesOf('Alba Button', module)
   component: AlbaButtonComponent,
   template: `
     <div style="display: flex; column-gap: .25rem;">
-      <alba-button size="large" >Large</alba-button>
-      <alba-button >Normal</alba-button>
+    <alba-button size="large" >Large</alba-button>
+    <alba-button >Normal</alba-button>
       <alba-button size="small">Small</alba-button>
     </div>
   `,
@@ -63,7 +63,7 @@ storiesOf('Alba Button', module)
   component: AlbaButtonComponent,
   template: `
     <div style="display: flex; column-gap: .25rem;">
-      <alba-button outline (click)="onClick()" >Primary Outline</alba-button>
+    <alba-button outline (click)="onClick()" >Primary Outline</alba-button>
       <alba-button outline variant="secondary">Secondary Outline</alba-button>
       <alba-button outline variant="success">Success Outline</alba-button>
       <alba-button outline variant="info">Info Outline</alba-button>
---
+++ projects/alba-ui/tailwind.config.js
@@ -0,0 +1,69 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'alba-white': '#fff',
+      'alba-black': '#111',
+      'alba-dark-gray': '#313335',
+      'alba-primary': {
+        100: '#20a8d8',
+        200: '#1b8eb7',
+        300: '#1985ac',
+        400: '#205493'
+      },
+      'alba-secondary': {
+        100: '#a4b7c1',
+        200: '#8da5b2',
+        300: '#869fac',
+        400: '#a4b7c1'
+      },
+      'alba-success': {
+        100: '#86c38b',
+        200: '#67ad38',
+        300: '#61a434',
+        400: '#53a451'
+      },
+      'alba-info': {
+        100: '#67c2ef',
+        200: '#44b4eb',
+        300: '#39afea',
+        400: '#5087a0'
+      },
+      'alba-warning': {
+        100: '#fabb3d',
+        200: '#f9ae18',
+        300: '#f9aa0b',
+        400: '#ce713c'
+      },
+      'alba-danger': {
+        100: '#ff5454',
+        200: '#ff2e2e',
+        300: '#ff2121',
+        400: '#cb444a'
+      },
+      'alba-light': {
+        100: '#f0f3f5',
+        200: '#f0f3f5',
+        300: '#d9e1e6',
+        400: '#d9e1e6'
+      },
+      'alba-dark': {
+        100: '#29363d',
+        200: '#1a2226',
+        300: '#151b1f',
+        400: '#29363d'
+      },
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+/**
+ 100: '#fabb3d',
+ 200: '#f9ae18',
+ 300: '#f9aa0b',
+ 400: '#ce713c'
+ */
